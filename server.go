bcfd1e5d6ce373081814bbb29a09bbe0362e8763
ryoga-dre <ryo_gushiken_r@dreamarts.co.jp>
2021-06-25 14:19:53 +0900

    feat: ブラウザで作成したhtmlを表示

--- server.go
+++ server.go
@@ -1,47 +1,145 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	tcpAddr, error := net.ResolveTCPAddr("tcp", "0.0.0.0:8089")
-	if error != nil {
-		log.Println("ResolveTCPAddr", error)
+var (
+	portOption = flag.Int("p", 8089, "help message for \"p\" option")
+)
+
+func handleResolveAddress(port string) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:"+port)
+	if err != nil {
+		log.Println("ResolveTCPAddr", err)
+		log.Println("handleResolveAddress", err)
 		return
 	}
 
-	listener, error := net.ListenTCP("tcp", tcpAddr)
-
-	if error != nil {
-		log.Println("ListenTCP", error)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Println("ListenTCP", err)
+		log.Println("handleResolveAddress", err)
 		return
 	}
 
-	fmt.Printf("Server running at 0.0.0.0:8089\n\n")
+	err = handleListener(listener)
+	if err != nil {
+		log.Println("handleListener", err)
+		log.Println("handleResolveAddress", err)
+		return
+	}
+}
 
+func handleListener(listener *net.TCPListener) error {
 	defer listener.Close()
 
 	for {
-		connection, error := listener.AcceptTCP()
+		connection, err := listener.AcceptTCP()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok {
+				if ne.Temporary() {
+					log.Println("AcceptTCP", err)
+					continue
+				}
+			}
+			log.Println("Unrecoverable", err)
+			return err
+		}
+		go handleConnection(connection)
+	}
+}
 
-		if error != nil {
-			log.Println("AcceptTCP", error)
+func handleConnection(connection *net.TCPConn) {
+	defer connection.Close()
+
+	fmt.Println(">>> start")
+	createHeader(connection)
+	fmt.Println(">>> end" + "\n")
+}
+
+func createHeader(connection *net.TCPConn) {
+	buf := make([]byte, 4096)
+	header := make(map[string]string)
+
+	n, err := connection.Read(buf)
+
+	if err != nil {
+		log.Println("Read", err)
+		return
+	}
+
+	receive := strings.TrimSpace(string(buf[:n]))
+	fmt.Println(receive + "\n")
+
+	h := strings.Split(receive, "\r\n")
+	request := strings.Split(h[0], " ")
+	header["method"] = request[0]
+	header["path"] = request[1]
+
+	for i, v := range h {
+		if i == 0 {
+			continue
+		}
+		headerFields := strings.Split(v, ": ")
+		header[headerFields[0]] = headerFields[1]
+	}
+	createResponce(connection, header)
+}
+
+func createResponce(connection *net.TCPConn, header map[string]string) {
+	method, ok := header["method"]
+	if !ok {
+		log.Println("no method found")
+		return
+	}
+
+	var resp []byte
+	if method == "GET" {
+		path, ok := header["path"]
+		if !ok {
+			log.Println("no path found")
+			return
+		} else if path == "/favicon.ico" {
+			log.Println("not create favicon.ico")
+			return
+		} else if path == "/" {
+			path = "/sample.html"
+		}
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Println("Getwd", err)
 			return
 		}
 
-		go func(connection net.Conn) {
-			defer connection.Close()
-
-			buf := make([]byte, 4096)
-			connection.Read(buf)
-
-			connection.Write([]byte("HTTP/1.1 200 OK\r\n"))
-			connection.Write([]byte("Content-Type: text/plain\r\n"))
-			connection.Write([]byte("\r\n"))
-			connection.Write([]byte("Hello, world"))
-		}(connection)
+		p := filepath.Join(cwd, filepath.Clean(path))
+		resp, err = ioutil.ReadFile(p)
+		if err != nil {
+			log.Println("ReadFile", err)
+			return
+		}
 	}
+
+	connection.Write([]byte("HTTP/1.1 200 OK\r\n"))
+	connection.Write([]byte("Content-Type: text/html\r\n"))
+	connection.Write([]byte("\r\n"))
+	connection.Write(resp)
+}
+
+func main() {
+	flag.Parse()
+	port := strconv.Itoa(*portOption)
+
+	log.Println("Server running at 0.0.0.0:" + port + "\n")
+
+	handleResolveAddress(port)
 }
